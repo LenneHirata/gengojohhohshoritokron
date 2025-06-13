3f8f3d3a4dc9e2de42f52ec0c82da8683a6f1459
50m-regent <len.hirata@gmail.com>
2025-06-13 19:19:10 +0900

    fasten search

--- models/hirata_search.py
+++ models/hirata_search.py
@@ -1,4 +1,4 @@
-import itertools
+from copy import deepcopy
 
 from tqdm import tqdm
 
@@ -76,30 +76,74 @@ class SearchDecipher(DecipherBase):
         print(self.c2n_map.c2n)
 
     def obtain_words(self, dataset: list[Data]) -> None:
-        words = set([word for data in dataset for word in data.text.split()])
+        words = [word for data in dataset for word in data.text.split()]
+        word_count = {}
+        for word in tqdm(words):
+            if word not in word_count:
+                word_count[word] = 0
+            word_count[word] += 1
 
+        word_count_count = {}
+        for count in word_count.values():
+            if count not in word_count_count:
+                word_count_count[count] = 0
+            word_count_count[count] += 1
+
+        print(sorted(word_count_count.items(), key=lambda x: x[0]))
+
+        words = set(words)
         self.words = {}
-        for word in words:
+        obtained_words_count = 0
+        for word in tqdm(words):
             if len(word) not in self.words:
-                self.words[len(word)] = []
-            self.words[len(word)].append(word)
+                self.words[len(word)] = {}
 
-        print(f"Obtained {len(words)} words")
+            current_dict = self.words[len(word)]
+            for i, char in enumerate(word):
+                if char not in current_dict:
+                    current_dict[char] = {}
+
+                if i == len(word) - 1:
+                    if word_count[word] > 3:
+                        current_dict[char] = word_count[word]
+                        obtained_words_count += 1
+                    else:  # 出現回数が3回以下の単語は省く
+                        assert isinstance(current_dict, dict)
+                        current_dict.pop(char)
+                    break
+
+                current_dict = current_dict[char]
+
+        print(f"Obtained {obtained_words_count} words")
+
+    @staticmethod
+    def __search(current_dict: dict, current_word: str, candidate_chars_list: list[list[str]]) -> list[str]:
+        words = []
+        for char in candidate_chars_list[0]:
+            if char not in current_dict:
+                continue
+            if isinstance(current_dict[char], int):
+                words.append(current_word + char)
+                continue
+            words.extend(SearchDecipher.__search(current_dict[char], current_word + char, candidate_chars_list[1:]))
+
+        return words
 
     def __search_and_fix(self, word: str) -> str:
         """
-        self.wordsに存在する単語のうち、最もwordと近いものを返す
+        self.wordsに存在する単語のうち、wordから複合されうるものを返す
         """
-        candidate_chars = [self.n2c[int(char)] for char in word]
+        if len(word) not in self.words:
+            return word
 
-        for candidate in self.words[len(word)]:
-            for char in candidate:
-                if char not in candidate_chars:
-                    break
-            else:
-                return candidate
+        candidate_chars_list = [self.n2c[int(char)] for char in word]
 
-        return word
+        words = self.__search(self.words[len(word)], "", candidate_chars_list)
+
+        if not words:
+            return word
+
+        return words[0]
 
     def perform(self, text: str) -> str:
         result = []
